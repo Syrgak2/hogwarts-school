d67885bc3c74447fb1185fcba3828b70de48fc90
Syrgak <Kary6ekov.s@gmail.com>
2024-01-27 14:05:14 +0100

    added sql query

---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,5 @@
+SELECT * FROM student WHERE age >= 10 and age <= 20;
+SELECT name FROM student;
+SELECT * FROM student WHERE name LIKE '%g%';
+SELECT * FROM student WHERE age < id;
+SELECT * FROM student ORDER BY age;
