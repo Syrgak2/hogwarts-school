48e47ab5d7fe47e9ad74c9f95380c17272319740
Syrgak <Kary6ekov.s@gmail.com>
2024-01-29 19:02:58 +0100

    refactored using lombok

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -2,11 +2,13 @@ package ru.hogwarts.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Data
 public class Faculty {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,53 +18,10 @@ public class Faculty {
     @OneToMany(mappedBy = "faculty")
     @JsonIgnore
     private List<Student> students;
-    public Faculty() {
-    }
 
     public Faculty(Long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public List<Student> getStudents() {
-        return students;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Faculty faculty = (Faculty) o;
-        return Objects.equals(id, faculty.id) && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
     }
 }
