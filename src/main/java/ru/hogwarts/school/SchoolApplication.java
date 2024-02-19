0d1bf01cf052e76f9ac2b86c9d00febbb2c8fd03
Syrgak <Kary6ekov.s@gmail.com>
2024-02-19 19:50:45 +0100

    homework-4.3 has been completed

--- src/main/java/ru/hogwarts/school/SchoolApplication.java
+++ src/main/java/ru/hogwarts/school/SchoolApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition
-
 public class SchoolApplication {
 
 	public static void main(String[] args) {
